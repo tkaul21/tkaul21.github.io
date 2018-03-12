a0bb793016f7dbe190b28bf325f480d336d708a5
M-Dawg <mehere@Meehirs-MacBook-Air.local>
2018-03-12 15:50:54 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+var myVariable = 20
+var myWord = "snow"
+
+// this is my function
+function saySomething(){
+
+    var result = my
+    console.log("This is jake typing" + myVariable)
+}
+function toggleDoge (){
+    
+    var img = document
+
+    //Revealing the dog
+    if(showDog == false){
+
+    }else if(showDog == true){
+
+    }
+
+}
